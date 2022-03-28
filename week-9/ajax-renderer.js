b2e1743dfff81e788716f83075ce6e0763f41e9e
Botlle <harshithatummala018@gmail.com>
2022-03-28 13:32:01 -0400

    Update ajax-renderer.js

--- week-9/ajax-renderer.js
+++ week-9/ajax-renderer.js
@@ -1,48 +1,45 @@
-const loadData = (path) =>
-  new Promise((resolve) => {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = ({ target }) => {
-      if (target.readyState == 4 && target.status == 200) {
-        resolve(JSON.parse(target.response));
-      }
-    };
-    xhttp.open("GET", path, true);
-    xhttp.send();
+const users = [
+
+  { username: `user1`, email: `user1@email.com` },
+  
+  { username: `user2`, email: `user2@email.com` }
+  
+  ];
+  
+  
+  
+  const getUser = (username) =>
+  
+  Promise((resolve) => {
+  
+  // get user data by username from users array
+  
   });
-
-const renderTable = (data, nameTerm) => {
-  const tableBody = document.getElementById("table-body");
-
-  if (!tableBody) {
-    throw new Error("No table element found");
-  }
-
-  let source = data;
-
-  if (nameTerm) {
-    source = source.filter(({ name }) => name.toLowerCase().includes(nameTerm));
-  }
-
-  const rows = source.reduce(
-    (acc, { id, name, value }) =>
-      acc +
-      `<tr id="table-row-${id}"><td>${id}</td><td>${name}</td><td>${value}</td></tr>`,
-    ``
-  );
-
-  tableBody.innerHTML = rows;
-};
-
-loadData(`./data.json`).then((data) => renderTable(data));
-
-const onSubmit = (event) => {
-  event.preventDefault();
-
-  const term = event.target.name.value;
-
-  loadData(`./data.json`).then((data) => renderTable(data, term));
-};
-
-const onReset = () => {
-  loadData(`./data.json`).then((data) => renderTable(data));
-};
+  
+  
+  
+  const getUsers = (userNames) => {
+  
+  // get all users for usernames passed as argument
+  
+  };
+  
+  
+  
+  const main = () => {
+  
+  const userNames = [`user1`, `user2`];
+  
+  
+  
+  const users = getUsers(userNames);
+  
+  
+  
+  console.log(users);
+  
+  };
+  
+  
+  
+  main();
